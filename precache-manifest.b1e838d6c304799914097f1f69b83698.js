de91d6fa432faa42c090e99137b6ed10f6738444
SAmira <amirashawkym23@gmail.com>
2020-06-03 06:46:24 -0700

    Updates

---
+++ precache-manifest.b1e838d6c304799914097f1f69b83698.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c19da101f36395142502073bf226de",
+    "url": "/AdidasHomePage/index.html"
+  },
+  {
+    "revision": "3ea6f3178e8621e72992",
+    "url": "/AdidasHomePage/static/css/2.6d10d21b.chunk.css"
+  },
+  {
+    "revision": "2d2f9ce1bc0ec605fbcf",
+    "url": "/AdidasHomePage/static/css/main.1238d8d9.chunk.css"
+  },
+  {
+    "revision": "3ea6f3178e8621e72992",
+    "url": "/AdidasHomePage/static/js/2.f55cb841.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdidasHomePage/static/js/2.f55cb841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2f9ce1bc0ec605fbcf",
+    "url": "/AdidasHomePage/static/js/main.a4b115d6.chunk.js"
+  },
+  {
+    "revision": "0aa2d451dfe420266fc9",
+    "url": "/AdidasHomePage/static/js/runtime-main.70a220bc.js"
+  },
+  {
+    "revision": "e2f5354712b150e4d27ef1b331c63a0f",
+    "url": "/AdidasHomePage/static/media/NewsletterBG.e2f53547.png"
+  },
+  {
+    "revision": "170cdac5e994500b881b9f588d8bf21a",
+    "url": "/AdidasHomePage/static/media/category_papper.170cdac5.png"
+  },
+  {
+    "revision": "35a44ec02757cefd15fe73f334f1a262",
+    "url": "/AdidasHomePage/static/media/header.35a44ec0.png"
+  },
+  {
+    "revision": "bd707f7450b3ed556cacc71bbf86633a",
+    "url": "/AdidasHomePage/static/media/icon-adidas-logo.bd707f74.svg"
+  },
+  {
+    "revision": "6a5c76d7d2eb9ca7ea3cb17b7061e83b",
+    "url": "/AdidasHomePage/static/media/icon-crosslink-logo-reebok.6a5c76d7.svg"
+  },
+  {
+    "revision": "1c8f4fa2b30cfd9a1aad931a32a7feb1",
+    "url": "/AdidasHomePage/static/media/img_1.1c8f4fa2.png"
+  },
+  {
+    "revision": "2a8f9917a438fadc693fcd643e408c2d",
+    "url": "/AdidasHomePage/static/media/img_2.2a8f9917.png"
+  },
+  {
+    "revision": "3223ef02c55d4ef4c3769e57958b8b0a",
+    "url": "/AdidasHomePage/static/media/mall_1.3223ef02.png"
+  },
+  {
+    "revision": "02df2ade788273a344ab5fc765fa5966",
+    "url": "/AdidasHomePage/static/media/mall_papper_2.02df2ade.png"
+  },
+  {
+    "revision": "0122bfacc8864a2910473a3831ca6936",
+    "url": "/AdidasHomePage/static/media/new_arrival_1.0122bfac.png"
+  },
+  {
+    "revision": "df0bf2cd1a89cd94156c1b72dde45bcb",
+    "url": "/AdidasHomePage/static/media/new_styles_1.df0bf2cd.png"
+  },
+  {
+    "revision": "6d71c3059a63d21e88ab9d37cf4fdabe",
+    "url": "/AdidasHomePage/static/media/new_styles_2.6d71c305.png"
+  },
+  {
+    "revision": "5ffba48b4917cd276ec90c563c200c98",
+    "url": "/AdidasHomePage/static/media/new_styles_3.5ffba48b.png"
+  },
+  {
+    "revision": "ccda069d6f56c0142ec76a9235b616d7",
+    "url": "/AdidasHomePage/static/media/news_1.ccda069d.png"
+  },
+  {
+    "revision": "09b4f20f51149d65dbb0b94cb7dc1101",
+    "url": "/AdidasHomePage/static/media/news_2.09b4f20f.png"
+  },
+  {
+    "revision": "7b2652d3a46a738c8c1fa55fabef551b",
+    "url": "/AdidasHomePage/static/media/neww_arrival_2.7b2652d3.png"
+  },
+  {
+    "revision": "dea61968557013d4a8277946d38c1749",
+    "url": "/AdidasHomePage/static/media/product_1.dea61968.png"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/AdidasHomePage/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/AdidasHomePage/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/AdidasHomePage/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "8386b8a781a6c206b3889b407906064c",
+    "url": "/AdidasHomePage/static/media/shoppin_gategory_card_1.8386b8a7.png"
+  },
+  {
+    "revision": "eb213eb05cb1c7fef4d391e985f91c5f",
+    "url": "/AdidasHomePage/static/media/shopping_type_card_1.eb213eb0.png"
+  },
+  {
+    "revision": "ae99b09cf5f63af1a283eb0ae1ad9e5b",
+    "url": "/AdidasHomePage/static/media/shopping_type_card_2.ae99b09c.png"
+  },
+  {
+    "revision": "ab7ee27396b3964b22abb4fd0d1a715d",
+    "url": "/AdidasHomePage/static/media/shopping_type_card_3.ab7ee273.png"
+  },
+  {
+    "revision": "e7fdf8acb331348866d6d82cd9513952",
+    "url": "/AdidasHomePage/static/media/video_img.e7fdf8ac.png"
+  },
+  {
+    "revision": "e59cc3d6b9cb8bff5bfcc8bb28967d33",
+    "url": "/AdidasHomePage/static/media/video_papper.e59cc3d6.png"
+  }
+]);
